656c3681f0b3ca19e99dfc64efaae87afa5eb70a
Zi Ting Chou <A88551212@gmail.com>
2022-09-27 23:53:20 +0800

    Update train_intent.py

--- train_intent.py
+++ train_intent.py
@@ -47,13 +47,7 @@ def main(args):
         # TODO: Training loop - iterate over train dataloader and update model weights
         for idx, batch in enumerate(train_dataloader):
             batch['text'] = vocab.encode_batch([i.split() for i in batch['text']])
-            print(type(batch['text']))
-            print(batch['text'])
             batch['text'] = torch.Tensor(batch['text'], dtype=torch.int32)
-            print(type(batch['text']))
-            print(batch['text'])
-            print(type(batch['text'][0]))
-            print(type(batch['intent']))
             prediction = model(batch["text"])
             print(prediction)
         # TODO: Evaluation loop - calculate accuracy and save model weights
