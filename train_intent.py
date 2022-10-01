202be30da63512c18359e87924c0f307bc4dfa36
Zi Ting Chou <A88551212@gmail.com>
2022-10-01 11:59:13 +0800

    Update train_intent.py

--- train_intent.py
+++ train_intent.py
@@ -70,7 +70,8 @@ def main(args):
             # print(batch['intent'])
             loss = criterion(prediction,batch['intent'])
             acc = count_acc(prediction, batch['intent'])
-            print(loss,acc)
+            # print(loss.item(),acc)
+            print(prediction)
             loss.backward()
             optimizer.step()
             
