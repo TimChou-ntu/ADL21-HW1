849342130ebd4643ff9e0f3dc6f1c33cb5914e73
Zi Ting Chou <A88551212@gmail.com>
2022-10-01 15:01:16 +0800

    Update model.py

--- model.py
+++ model.py
@@ -22,11 +22,11 @@ class SeqClassifier(torch.nn.Module):
             # torch.nn.Linear(1024,512),
             # torch.nn.ReLU(inplace=True),
             torch.nn.Dropout(),
-            torch.nn.Linear(2048,1024),
-            torch.nn.ReLU(inplace=True),
             torch.nn.Linear(1024, 512),
-            torch.nn.ReLU(inplace=True),
-            torch.nn.Linear(512,150)
+            torch.nn.ReLU(),
+            torch.nn.Linear(512,256),
+            torch.nn.ReLU(),
+            torch.nn.Linear(256,150)
             )
     @property
     def encoder_output_size(self) -> int:
