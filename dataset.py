a87dae0777e2c2083b7383304bb22f2c00abb896
Zi Ting Chou <A88551212@gmail.com>
2022-10-07 13:09:07 +0800

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -47,7 +47,7 @@ class SeqClsDataset(Dataset):
         id = [i['id'] for i in samples]
         if self.train:
             intent = [i['intent'] for i in samples]
-            return {"text":torch.Tensor(text),"id":id,"intent":torch.Tensor(intent).int()}
+            return {"text":torch.Tensor(text),"id":id,"intent":torch.Tensor(intent).long()}
         else:
             return {"text":torch.Tensor(text),"id":id}
 
