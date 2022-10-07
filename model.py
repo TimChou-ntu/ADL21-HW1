86bcfee6128fad0a803658dba574a9760b1e07bf
Zi Ting Chou <A88551212@gmail.com>
2022-10-07 21:04:13 +0800

    Update model.py

--- model.py
+++ model.py
@@ -28,6 +28,9 @@ class SeqClassifier(torch.nn.Module):
             torch.nn.Linear(hidden_size*2,hidden_size),
             torch.nn.ReLU(),
             torch.nn.Dropout(0.1),
+            torch.nn.Linear(hidden_size,hidden_size),
+            torch.nn.ReLU(),
+            torch.nn.Dropout(0.1),
             torch.nn.Linear(hidden_size,num_class)
             )
         self.init_weight()
