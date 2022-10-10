db160ebaf48e012d22d45150aa7c71a6e3bf74ed
Zi Ting Chou <A88551212@gmail.com>
2022-10-10 13:58:18 +0800

    Update train_intent.sh

--- train_intent.sh
+++ train_intent.sh
@@ -1,3 +1,4 @@
+export CUDA_VISIBLE_DEVICES=1
 python train_elmo.py --data_dir=./data/intent/ --cache_dir=./cache/intent/ --ckpt_dir=./ckpt/intent/ --mode=intent
 python train_intent.py
 bash inten_cls.sh
