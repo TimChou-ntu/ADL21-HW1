808f2fb4ae52c3e156570559bac9c42616aded55
Zi Ting Chou <A88551212@gmail.com>
2022-10-10 13:57:10 +0800

    train.sh

---
+++ train_intent.sh
@@ -0,0 +1,3 @@
+python train_elmo.py --data_dir=./data/intent/ --cache_dir=./cache/intent/ --ckpt_dir=./ckpt/intent/ --mode=intent
+python train_intent.py
+bash inten_cls.sh
---
+++ train_slot.sh
@@ -0,0 +1,3 @@
+python train_elmo.py --data_dir=./data/slot/ --cache_dir=./cache/slot/ --ckpt_dir=./ckpt/slot/ --mode=slot
+python train_slot.py
+bash slot_tag.sh
